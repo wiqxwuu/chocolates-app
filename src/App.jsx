68bc175df23f6c2183df7563d099d27d05ed86f9
Asema <asema.abdurasulova@gmail.com>
2024-12-08 23:03:27 +0600

    started new project

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import './App.css'
+
+let products = [
+  {
+    name: "Babyfox",
+    price: 45,
+    discount: false,
+    present: 0,
+    newPrice: 45,
+    image: "https://basket-01.wbbasket.ru/vol131/part13145/13145112/images/big/1.webp"
+  },
+  {
+    name: "Kit Kat",
+    price: 80,
+    discount: true,
+    present: 5,
+    newPrice: 76,
+    image: "https://content2.rozetka.com.ua/goods/images/big/462583856.jpg"
+  },
+  {
+    name: "Alpen Gold",
+    price: 90,
+    discount: true,
+    present: 10,
+    newPrice: 81,
+    tastes: ["Молочный", "Капучино", "Клубничный", "Черничный", "Арахис и Кукурузные хлопья", "Вишня и Миндаль", "Солёный арахис и Крекер", "Тёмный", "Тёмный, Малина и Йогурт", "Солёный миндаль и Карамель", "Двойной шоколад", "Фундук и Изюм", "Фундук", "Кокос и Миндаль", "Орео"],
+    image: "https://basket-13.wbbasket.ru/vol1928/part192890/192890459/images/big/1.webp"
+  },
+  {
+    name: "Snikers",
+    price: 75,
+    discount: false,
+    present: 0,
+    newPrice: 75,
+    tastes: ["Классический", "Криспер", "Тёмный", "С семечками", "Криспер Трио", "С миндалем", "Белый", "Лесной орех", "Пломбир"],
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2ayfk2CUNBrGcNjpLlKN3X-Xh7msWUKyneQ&s"
+  },
+  {
+    name: "Milki Way",
+    price: 80,
+    discount: false,
+    present: 0,
+    newPrice: 80,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe9ILXU4jwBmDwzJIh3Bvcllqorn1OSyGZSRPMTBwwGlam3C4b_c7XRZGHQevoJNmubiQ&usqp=CAU"
+  },
+  {
+    name: "Twix",
+    price: 93,
+    discount: true,
+    present: 5,
+    newPrice: 88,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNsAQB8uxC4lIIgw6e3RGU1da6Lby4TWLlmQ&s"
+  },
+  {
+    name: "Bounty",
+    price: 95,
+    discount: true,
+    present: 12,
+    newPrice: 83,
+    tastes: ["Классический", "Райский ананас", "Райское манго", "Тёмный"],
+    image: "https://basket-02.wbbasket.ru/vol267/part26742/26742351/images/big/1.webp"
+  },
+  {
+    name: "Nesquick",
+    price: 190,
+    discount: true,
+    present: 10,
+    newPrice: 171,
+    image: "https://basket-02.wbbasket.ru/vol254/part25422/25422840/images/big/1.webp"
+  }
+]
+
+function App() {
+
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default App
